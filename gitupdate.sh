0a781a1a3ff0c4aa98a165a6e6d4a29d351e7c97
Mattias <martikainen@live.se>
2017-03-14 10:17:50 +0100

    Minor

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
 cd /home/homeassistant/.homeassistant/
-source /srv/homeassistant/homeassistant_venv/bin/activate
-hass --script check_config
-
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update]
-read CHANGE_MSG
-git commit -m "${CHANGE_MSG}"
+git commit
 git push origin master
 
 exit
